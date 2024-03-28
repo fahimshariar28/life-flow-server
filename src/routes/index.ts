cbfe3714da59a1592c004239053231894141eb5b
fahimshariar28 <fahimshariar28@gmail.com>
2024-03-29 03:14:12 +0600

    userProfile route added in main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { userRoutes } from "../modules/User/user.route";
 import { authRoutes } from "../modules/Auth/auth.route";
 import { requestRoutes } from "../modules/Request/request.route";
+import { userProfileRoutes } from "../modules/UserProfile/userProfile.route";
 
 const router = express.Router();
 
@@ -14,6 +15,9 @@ const moduleRoutes = [
   },
   {
     route: requestRoutes,
+  },
+  {
+    route: userProfileRoutes,
   },
 ];
 
