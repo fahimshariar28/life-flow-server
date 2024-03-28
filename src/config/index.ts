23cc57df8346c647da99e0cb89cd770ad4208ffa
fahimshariar28 <fahimshariar28@gmail.com>
2024-03-28 16:28:00 +0600

    JWT access token secret and expires added in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,8 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  jwt: {
+    secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
+  },
 };
